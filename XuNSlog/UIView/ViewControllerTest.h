bab078cf13960822e5ecc55b9c30c3d609a02b75
xuzhenguo <xuzhenguo@yzyy365.com>
2018-03-16 15:04:05 +0800

    sn_farmer_xuzhenguo v 0.1.0

---
+++ XuNSlog/UIView/ViewControllerTest.h
@@ -0,0 +1,12 @@
+//
+//  ViewControllerTest.h
+//  Pods-sn_farmer_xuzhenguo_Example
+//
+//  Created by mac on 2018/3/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerTest : UIViewController
+-(void)test;
+@end
---
+++ XuNSlog/UIView/ViewControllerTest.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerTest.m
+//  Pods-sn_farmer_xuzhenguo_Example
+//
+//  Created by mac on 2018/3/15.
+//
+
+#import "ViewControllerTest.h"
+
+@interface ViewControllerTest ()
+
+@end
+
+@implementation ViewControllerTest
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+-(void)test{
+    NSLog(@"test我来了");
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
